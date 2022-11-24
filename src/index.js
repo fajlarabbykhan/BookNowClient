ad7240c06e174cd57a94ddf979d0a09059dffa01
Fajla Rabby Khan <fajlarabbyk55@gmail.com>
2022-11-24 09:50:15 +0600

    working on search component

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { SearchContextProvider } from "./context/SearchContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <SearchContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SearchContextProvider>
   </React.StrictMode>
 );
 
